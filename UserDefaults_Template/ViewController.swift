d7f2c74e6289b1a865d7abafc0de0efe3ba5567d
s4rv4d <sarvadshetty@gmail.com>
2018-03-16 10:47:03 +0530

    Initial Commit

---
+++ UserDefaults_Template/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UserDefaults_Template
+//
+//  Created by Sarvad shetty on 3/16/18.
+//  Copyright © 2018 Sarvad shetty. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
